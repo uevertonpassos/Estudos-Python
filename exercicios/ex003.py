0d533b22969ecb4df3e4c7402ae663365a8ab329
Ueverton Passos <uevertonpassos96@gmail.com>
2021-07-12 11:09:52 -0300

    Create ex003.py

---
+++ exercicios/ex003.py
@@ -0,0 +1,6 @@
+# Lendo números e somando
+
+num1 = int(input('Digite um valor: '))
+num2 = int(input('Digite outro valor: '))
+soma = num1 + num2
+print('A soma entre {} e {} é {}'.format(num1, num2, soma))
