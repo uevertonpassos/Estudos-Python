c569bce44954f01a5cb9937d2b0a644d0a2a7369
Ueverton Passos <uevertonpassos96@gmail.com>
2021-07-12 11:09:09 -0300

    Create tiposprimitivos.py

---
+++ aulas/tiposprimitivos.py
@@ -0,0 +1,5 @@
+# int - para números inteiros ex: 7, -4, 0, 9875
+# float - para números flutuantes ex: 4.5, 0.076, -15.645, 7.0
+# bool - para números booleanos ex: True, False
+# str - para strings (ja vem como padrão) ex: tudo que estiver entre aspas "7.4", '74'
+
