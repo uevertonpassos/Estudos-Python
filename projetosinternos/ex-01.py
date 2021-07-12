69ea41cdd31d104a09f05aaab230d31b52fc6323
Ueverton Passos <uevertonpassos96@gmail.com>
2021-07-12 12:40:12 -0300

    Create ex-01.py

---
+++ projetosinternos/ex-01.py
@@ -0,0 +1,28 @@
+import random
+from time import sleep
+print('==#=='*5)
+print(' Perguntas e Respostas!')
+print('==#=='*5)
+sleep(1)
+inicio = input('Pressione enter para começar!')
+sleep(1)
+print('Iniciando em...')
+sleep(1)
+print('3...')
+sleep(1)
+print('2...')
+sleep(1)
+print('1...')
+sleep(1)
+print('Primeira pergunta...')
+sleep(1)
+p1 = input('Cor do cavalo branco de Napoleão: ') # ?????????????
+if p1 == 'branco':
+    print('A resposta está correta!')
+else:
+    print('A resposta está incorreta!')
+p2 = input('Formato geométrico de uma bola:')
+if p2 == 'esfera':
+    print('A resposta está correta!')
+else:
+    print('A resposta está incorreta!')
