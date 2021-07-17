b7100a91d952bdffdbade3efb527d5b907d770ec
Ueverton Passos <uevertonpassos96@gmail.com>
2021-07-17 15:43:06 -0300

    Create ex005.py

---
+++ exercicios/ex005.py
@@ -0,0 +1,10 @@
+# Mostrando o Antecessor e o Sucessor de um número!
+
+num = int(input('Digite um valor: '))
+ant = num - 1
+suc = num + 1
+print('O antecessor de {} é {} e o seu sucessor é {}!'.format(num, ant, suc))
+
+#ou
+# num = int(input('Digite um valor: '))
+# print('O antecessor de {} é {} e o seu sucessor é {}!'.format(num, (num-1), (num+1)))
