4a03009507cbda3c8a9bee07ff71ef0fabd9ee84
Ueverton Passos <uevertonpassos96@gmail.com>
2021-07-23 17:19:43 -0300

    Create ex007.py

---
+++ exercicios/ex007.py
@@ -0,0 +1,6 @@
+# Tirando a Média Aritimética de notas escolares.
+
+notaUm = float(input('Digite a primeira nota do aluno: '))
+notaDois = float(input('Digite a segunda nota do aluno: '))
+media = (notaUm + notaDois) / 2
+print('A Média Aritimética do aluno foi: {} '.format(media))
