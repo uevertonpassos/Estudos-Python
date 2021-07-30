28cae3e2abbb2ceee653f5c051b386eb24273420
Ueverton Passos <uevertonpassos96@gmail.com>
2021-07-30 11:47:28 -0300

    Exercícios de python

--- exercicios/ex008.py
+++ exercicios/ex008.py
@@ -1,1 +1,4 @@
 # Conversor de Medidas  em Python
+real = float(input('Digite quanto você tem em Real'))
+dolar = real / 3.27
+print('Com {} Reais, você pode comprar {} Dolares!'.format(real, dolar))
