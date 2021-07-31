77a6d8f8486be9b527add23ad9d7792734cb5d9f
Ueverton Passos <uevertonpassos96@gmail.com>
2021-07-31 15:44:52 -0300

    testando commits

--- exercicios/ex010.py
+++ exercicios/ex010.py
@@ -1,4 +1,4 @@
 # Conversor de Moedas  em Python
-real = float(input('Digite quanto você tem em Real'))
-dolar = real / 3.27
-print('Com {} Reais, você pode comprar {} Dolares!'.format(real, dolar))
+real = float(input('Digite quanto você tem em Real: '))
+dolar = real / 5.21
+print('Com {} Reais, você pode comprar {:.2f} Dolares!'.format(real, dolar))
