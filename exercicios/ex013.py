bc697ae128ea613ea025de248c3071da7a2e455b
Ueverton Passos <uevertonpassos96@gmail.com>
2021-08-01 00:15:42 -0300

    exercicios de porcentagem entre outros

--- exercicios/ex013.py
+++ exercicios/ex013.py
@@ -2,4 +2,4 @@
 
 sal = float(input('Digite seu salário R$: '))
 aum = sal + (sal*15/100)
-print('O seu novo salário é de R$:{} '.format(aum))
+print('O seu novo salário é de R$:{:.2f} '.format(aum))
---
+++ exercicios/ex014.py
@@ -0,0 +1,5 @@
+# Transformando Celsius em Fahreheit
+c = float(input('Digite a quantidade de graus em Celsius: '))
+f = 9*c/5+32
+print('{}°C equivale a {}°F!'.format(c, f))
+
