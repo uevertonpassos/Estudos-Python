46745957fbbebfb875a7c9ad37a1688a11492bc4
Ueverton Passos <uevertonpassos96@gmail.com>
2021-08-01 21:54:11 -0300

    Exercícios com módulos

--- exercicios/ex018.py
+++ exercicios/ex018.py
@@ -2,4 +2,3 @@
 from math import sin, cos, tan, radians
 ang = int(input('Digite o ângulo: '))
 print('O seno de {} é: {:.2f}, o cosseno é: {:.2f} e a sua tangente é: {:.2f}'.format(ang, sin(radians(ang)), cos(radians(ang)), tan(radians(ang))))
-
