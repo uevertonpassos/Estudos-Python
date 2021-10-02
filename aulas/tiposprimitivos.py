c67a601cf888723195b61645a3e0b349ab2073b3
Ueverton Passos <uevertonpassos96@gmail.com>
2021-10-02 18:02:04 -0300

    Mais aulas

--- aulas/tiposprimitivos.py
+++ aulas/tiposprimitivos.py
@@ -3,3 +3,16 @@
 # bool - para números booleanos ex: True, False
 # str - para strings (ja vem como padrão) ex: tudo que estiver entre aspas "7.4", '74'
 
+# Exercício de verificações usando print
+
+# String: nome
+print('Ueverton', type('Ueverton'))
+# Idade: int
+print(25, type(25))
+print('25', type(int('25'))) # transformando str em int
+print(25.0, type(int(25.0))) # transformando float em int
+# Altura: float
+print(1.63, type(1.63))
+print('1.63', type(float('1.63')))# transformando str em float
+# É maior de idade: bool
+print(25, 25 > 18) # testando se é ou não verdadeiro que o numero é maior(TRUE)
