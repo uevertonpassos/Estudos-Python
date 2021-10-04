d874dd57b85dfc5d17b6011e0ddb8e3ad8de21a0
Ueverton Passos <uevertonpassos96@gmail.com>
2021-10-04 13:49:50 -0300

    update

--- aulas/aula005.py
+++ aulas/aula005.py
@@ -7,3 +7,4 @@ print('Adição + ')
 print('Subtração - ')
 print('Resto da divisão % ')
 print('Parenteses alteram a ordem de precedência ')
+# É importante deixar claro que a ordem de precedência é a mesma da matemática
