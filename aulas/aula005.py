2b7c2e33ed25d7f321a53a7a4b3f8f98839d5040
Ueverton Passos <uevertonpassos96@gmail.com>
2021-10-04 13:45:38 -0300

    Operadores aritméticos

---
+++ aulas/aula005.py
@@ -0,0 +1,9 @@
+# Operadores aritméticos 
+
+print("Multiplicação * ")
+print('Divisão / ')
+print('Divisão inteira // ')
+print('Adição + ')
+print('Subtração - ')
+print('Resto da divisão % ')
+print('Parenteses alteram a ordem de precedência ')
