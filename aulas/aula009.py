86440ba148ac9f9017fda5c16298917b7e9b2ded
Ueverton Passos <uevertonpassos96@gmail.com>
2021-10-16 11:50:35 -0300

    Len

---
+++ aulas/aula009.py
@@ -0,0 +1,14 @@
+# Aprendendo Len
+
+#Len é usado para calcular a quantidade de strings princpalmente utilizadas pelo usuário, exemplo:
+
+nome = input("Digite algo: ")
+qtd_nome = len(nome)
+print(qtd_nome)
+
+# Também pode ser usado como método, exemplo: 
+
+nome = input('Digite algo: ')
+print(nome.__len__())
+
+
