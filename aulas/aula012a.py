1ea73daea3694285478f131a3fff0d1bc60ba969
Ueverton Passos <uevertonpassos96@gmail.com>
2021-10-17 11:53:03 -0300

    Resolução de Exercício

--- aulas/aula012a.py
+++ aulas/aula012a.py
@@ -3,16 +3,14 @@
 # 1° Exercicio: pedir um input, informar se é par ou impar, e se não for numerico, avisar que não é
 
 num = input("Digite um número: ")
-if type(num) ==  str:
-    print("não é um número!")
-    num2 = input("Por favor digite um número: ")
-    resto = int(num2) % 2
-    if resto == 0: 
-        print("Este número é Par!")
+if num.isdigit():
+    if int(num) % 2 ==0:
+        print("Par")
     else:
-        print("Estu número é Ímpar")
+        print("Ímpar")
 else:
-    ...
+    print("Entrada inválida")
+
 
    
 
