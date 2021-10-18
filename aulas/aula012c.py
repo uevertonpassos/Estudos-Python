6f8c92a2c91649035323bed3dc3f5745761329f2
Ueverton Passos <uevertonpassos96@gmail.com>
2021-10-18 18:41:20 -0300

    Exercicio proposto 3

---
+++ aulas/aula012c.py
@@ -0,0 +1,12 @@
+# 3° Exercicio
+
+# Respostas diferentes para tamanho dos nomes
+
+nome = input("Digite o seu nome: ")
+if len(nome) <= 4:
+    print("Seu nome é muito curto!")
+elif len(nome) >= 5 and len(nome)<= 6:
+    print("Seu nome é normal!")
+elif len(nome) >= 7:
+    print("Seu nome é muito grande!")
+    
