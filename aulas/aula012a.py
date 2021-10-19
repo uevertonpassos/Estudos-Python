03bbb954db800898c4f02626d6c112ffd9039682
Ueverton Passos <uevertonpassos96@gmail.com>
2021-10-19 16:38:19 -0300

    Reformulação exercicio 1

--- aulas/aula012a.py
+++ aulas/aula012a.py
@@ -5,8 +5,22 @@
 num = input("Digite um número: ")
 if num.isdigit():
     if int(num) % 2 ==0:
-        print("Par")
+        print(f"O número {num} é Par!")
     else:
-        print("Ímpar")
+        print(f"O número {num} Ímpar")
 else:
     print("Entrada inválida")
+
+
+# ou
+
+num_usuario = input("Digite um número: ")
+
+if not num_usuario.isdigit():
+    print("Isso não é um número!")
+else:
+    num_usuario = int(num_usuario)
+    if not num_usuario % 2 == 0:
+        print(f"O número {num_usuario} é Ímpar!")
+    else:
+        print(f"O número {num_usuario} é Par!")
