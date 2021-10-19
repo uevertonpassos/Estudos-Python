a50ade797c36b86e8a8cffe1136bcaf291b1b94f
Ueverton Passos <uevertonpassos96@gmail.com>
2021-10-19 16:54:32 -0300

    Reformulando ex 03

--- aulas/aula012c.py
+++ aulas/aula012c.py
@@ -7,7 +7,17 @@ if len(nome) <= 4:
     print("Seu nome é muito curto!")
 elif len(nome) >= 5 and len(nome)<= 6:
     print("Seu nome é normal!")
-elif len(nome) >= 7:
+elif len(nome) >= 6:
     print("Seu nome é muito grande!")
     
-    
+
+# ou
+
+usuario = input("Digite o seu nome: ")
+tamanho = len(usuario)
+if tamanho <= 4:
+    print("Seu nome é muuito curto!")
+elif tamanho >= 5:
+    print("Seu nome é normal!")
+else:
+    print("Seu nome é muito grande! ")
