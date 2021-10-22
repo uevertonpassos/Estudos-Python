1e1a46af4888eeb75ed79b89df14eac7e10ff470
Ueverton Passos <uevertonpassos96@gmail.com>
2021-10-22 20:48:55 -0300

    formatações

---
+++ aulas/aula013.py
@@ -0,0 +1,7 @@
+# testando formatações: 
+
+num = 11
+
+print(f"{num:0>10}") #  preenchendo espaços à esquerda com zeros
+print(f"{num:0<10}") #  preenchendo espaços à direita com zeros
+print(f"{num:0^10}") #  colocando o número entre zeros
