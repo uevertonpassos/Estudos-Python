0402455e2ddfb4fb5dfa21c633d7cf7196619918
Ueverton Passos <uevertonpassos96@gmail.com>
2021-10-23 20:17:51 -0300

    Fatiamento

---
+++ aulas/aula014.py
@@ -0,0 +1,11 @@
+#  Fatiamento
+
+#  usado para pegar apenas a parte da string desejada, exemplo:
+
+nova_string = "Python s2"
+print(nova_string[1:9:2])
+
+# é dado por [] e separados por dois pontos :
+
+#  [1°: 2°: 3°] primeiro onde começamos a fatiar, o segundo para até onde fatiar, e o terceiro o step, para pular por exemplo de 2 em 2
+
