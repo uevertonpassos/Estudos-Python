4861a592fa8da971f15a8d0ac590b7442391f7bc
Ueverton Passos <uevertonpassos96@gmail.com>
2021-10-22 21:06:13 -0300

    formatações

--- aulas/aula013.py
+++ aulas/aula013.py
@@ -1,7 +1,13 @@
 # testando formatações: 
 
 num = 11
-
+nome = "Ueverton"
 print(f"{num:0>10}") #  preenchendo espaços à esquerda com zeros
 print(f"{num:0<10}") #  preenchendo espaços à direita com zeros
 print(f"{num:0^10}") #  colocando o número entre zeros
+
+print(nome.ljust(20, "#")) #  justificado à esquerda
+print(nome.rjust(20, "#")) #  justificado à direita
+print(nome.lower()) #  tudo em minusculo
+print(nome.upper()) #  tudo em maiúsculo
+
