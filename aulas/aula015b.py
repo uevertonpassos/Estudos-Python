4995892c4757c06182df6cd6d44a744a69ed8961
Ueverton Passos <uevertonpassos96@gmail.com>
2021-10-24 00:21:29 -0300

    mais while

---
+++ aulas/aula015b.py
@@ -0,0 +1,10 @@
+# Ainda While
+
+x = 0 # isso é um exemplo de coluna e linha, x é coluna
+while x < 10:
+    y = 0
+    while y < 5:
+        print(f"({x}, {y})")
+        y += 1
+    x += 1
+print("Acabou")
