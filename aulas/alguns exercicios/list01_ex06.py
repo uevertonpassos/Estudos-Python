e3b1a8fdc7470a864f3421bd124f4e557589fcb1
Ueverton Passos <uevertonpassos96@gmail.com>
2021-11-09 18:56:46 -0300

    lista 1 questão 6

--- aulas/alguns exercicios/list01_ex06.py
+++ aulas/alguns exercicios/list01_ex06.py
@@ -5,3 +5,4 @@ lado2 = int(input('Digite o segundo lado do triângulo: '))
 lado3 = int(input('Digite o terceiro lado do triãngulo: '))
 area = sqrt (((lado1+lado2+lado3) /2) * (((lado1+lado2+lado3)/2) - lado1) * (((lado1+lado2+lado3)/2) - lado2) * (((lado1+lado2+lado3)/2) - lado3))
 print(f'A área do triângulo é: {area:.2f}cm')
+
