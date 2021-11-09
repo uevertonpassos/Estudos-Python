29739410cc075eda7423e34238526414aa1cc839
Ueverton Passos <uevertonpassos96@gmail.com>
2021-11-09 18:57:22 -0300

    Lista 1 questão 7

--- aulas/alguns exercicios/list01_ex07.py
+++ aulas/alguns exercicios/list01_ex07.py
@@ -3,3 +3,4 @@ from math import hypot
 ad_cat = int(input('Digite o cateto adjacente: '))
 op_cat = int(input('Digite o cateto oposto: '))
 print(f'o valor da Hipotenusa será de {hypot(ad_cat, op_cat):.0f}')
+
