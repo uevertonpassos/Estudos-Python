6cd3169d4677b1655b5d9b7c66b2c9d53d4964be
Ueverton Passos <uevertonpassos96@gmail.com>
2021-11-09 18:54:03 -0300

    Questão 2 lista 1

--- aulas/alguns exercicios/list01_ex02.py
+++ aulas/alguns exercicios/list01_ex02.py
@@ -4,3 +4,4 @@ num3 = int(input('Digite o terceiro valor: '))
 num4 = int(input('Digite o quarto valor: '))
 media = (num1 + num2 + num3 + num4) / 4
 print(f'A média aritmética dos números é: {media}')
+
