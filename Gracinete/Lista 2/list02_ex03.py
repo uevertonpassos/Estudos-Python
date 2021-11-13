35d5bada541241615abcb68b73f70c1bd535b496
Ueverton Passos <uevertonpassos96@gmail.com>
2021-11-13 19:05:21 -0300

    exercicio 3 lista 2

---
+++ Gracinete/Lista 2/list02_ex03.py
@@ -0,0 +1,9 @@
+a = float(input('Digite o valor de A: '))
+b = float(input('Digite o valor de B: '))
+c = float(input('Digite o valor de C: '))
+
+if a < b +c  and b < a + c and c < a + b:
+    print('É possível formar um triângulo!')
+
+else: 
+    print('n é possivel formar um triangulo')
