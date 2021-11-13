971ebf44c51440917b55994b99d484ae0d819dff
Ueverton Passos <uevertonpassos96@gmail.com>
2021-11-13 17:49:23 -0300

    While/Else

---
+++ aulas/aula016.py
@@ -0,0 +1,13 @@
+# While com Else // treinando contadores e acumuladores
+
+contador = 1
+acumulador = 1
+
+while contador <= 10:
+    print(contador, acumulador)
+    #if contador > 5:
+        #break
+    acumulador = acumulador + contador
+    contador += 1
+else:
+    print("Cheguei no Else")
