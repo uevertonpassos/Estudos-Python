4bae56f187533666f80eef0e970c979843de05da
Ueverton Passos <uevertonpassos96@gmail.com>
2021-11-13 18:34:22 -0300

    resolvendo listas

---
+++ Gracinete/Lista 2/list02_ex02.py
@@ -0,0 +1,14 @@
+a = int(input('Digite o valor de A'))
+b = int(input('Digite o valor de B'))
+c = int(input('Digite o valor de C'))
+
+
+delta = (b**2 ) - 4 * a * c
+
+if a == 0:
+    print('O valor de A tem que ser maior que 0')
+elif delta < 0:
+    print("Não possui raizes reais")
+else:
+    X1 = (-b + delta ** (1 / 2)) / (2 * a)
+    x2 = (-b - delta ** (1 / 2)) / (2 * a)
