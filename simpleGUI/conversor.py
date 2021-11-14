0fdd7c67e3875ef0dbfd083db2c9e7de3567fc9e
Ueverton Passos <uevertonpassos96@gmail.com>
2021-11-14 17:58:24 -0300

    tentativa de criação de interfaces gráficas usando PySimpleGUI

---
+++ simpleGUI/conversor.py
@@ -0,0 +1,20 @@
+from tkinter.constants import NUMERIC
+from PySimpleGUI import PySimpleGUI as sg
+
+sg.theme('DarkTeal')
+
+layout = [ 
+    [sg.Text("Login: "), sg.Input(key=('Login'),size=(20,1))],
+    [sg.Text('Senha:'), sg.Input(key= ('Senha'), password_char='*',size=(20,1))],
+    [sg.Button("Enter")]
+]
+
+window = sg.Window('Faça Login', layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WINDOW_CLOSED:
+        break
+    if event == "Enter":
+        if values ['Login'] == 'Batata' and values ['Senha'] == '1234':
+            print('Bem vindo ao sistema!')
