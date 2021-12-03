a7d9503a2d70bfd07b5e6864c3b5e9be2187c750
Ueverton Passos <uevertonpassos96@gmail.com>
2021-12-03 11:08:26 -0300

    exercícios do curso do Guanabara

---
+++ Guanabara/exercicios/ex022.py
@@ -0,0 +1,6 @@
+nome = input('Digite o seu nome completo: ')
+print(f'Seu nome completo todo em maiúsculo é: {nome.upper()}')
+print(f'Seu nome completo todo em minúsculo é: {nome.lower()}')
+quantidade  = nome.split()
+print(f'Seu nome completo tem {len(quantidade[0]+ quantidade[1])}, letras!')
+
