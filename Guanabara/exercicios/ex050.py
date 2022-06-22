6f146c16a0a30f35fcffd77915ea891b5cfdd48f
Ueverton Passos <uevertonpassos96@gmail.com>
2022-06-22 05:01:37 -0300

    Exercício FOR

--- Guanabara/exercicios/ex050.py
+++ Guanabara/exercicios/ex050.py
@@ -1,8 +1,8 @@
 soma = 0
 cont = 0
-for par in range (1,7):
-    num = int(input('Digite um número: '))
-    if num % 2 ==0:
+for par in range(1, 7):
+    num = int(input('Digite um numero: '))
+    if num % 2 == 0:
         soma += num
         cont += 1
-print(f'Você digitoU{cont} NUMEROS PARES e a soma deles é {soma}')
+print(f'vc informou {cont} numeros pares e a soma entre eles é: {soma}')
