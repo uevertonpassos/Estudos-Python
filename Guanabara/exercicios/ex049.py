b7ca347d6d616e8a956965c8f8ccff6bd5132b52
Ueverton Passos <uevertonpassos96@gmail.com>
2022-06-22 04:36:41 -0300

    exercicio FOR

--- Guanabara/exercicios/ex049.py
+++ Guanabara/exercicios/ex049.py
@@ -1,5 +1,5 @@
 # tabuada v2.0
-num = int(input("Digite o número desejado: "))
-for tab in range (0, 11):
-    print(f'{num} x {tab} = {num*tab}')
+num = int(input('Digite o número desejado: '))
+for tab in range(0, 11):
+    print(f'{num} x {tab} = {num * tab}')
     
