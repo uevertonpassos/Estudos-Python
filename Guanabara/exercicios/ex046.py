365a7fa557fc4abe679aab86a6c076849fe1e5d1
Ueverton Passos <uevertonpassos96@gmail.com>
2022-06-22 03:48:43 -0300

    começando a estudar FOR

--- Guanabara/exercicios/ex046.py
+++ Guanabara/exercicios/ex046.py
@@ -1,5 +1,6 @@
 from time import sleep  
-for c in range(10, 0, -1):
+for c in range(10, -1, -1): #lembrar que o último número é sempre ignorado
     print(f'fogos estourando em {c}')
     sleep(1)
 print('FELIZ ANO NOVO!!!')
+
