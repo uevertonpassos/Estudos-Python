b01cc747fe644124c920619ce0b20273a7ee3f81
Ueverton Passos <uevertonpassos96@gmail.com>
2023-01-31 17:55:22 -0300

    exercicios while

---
+++ Guanabara/exercicios/ex057.py
@@ -0,0 +1,10 @@
+while True:
+    sexo = input("Digite o seu sexo: ").upper()
+    if sexo == "M":
+        print("Perfeito")
+        break
+    elif sexo == "F":
+        print("Perfeito")
+        break
+    else:
+        print("Digite novamente")
