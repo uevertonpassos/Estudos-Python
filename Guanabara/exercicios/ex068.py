0bd4135c77250a4f5c66a2e4debb862e58aa4462
Ueverton Passos <uevertonpassos96@gmail.com>
2023-02-02 19:26:04 -0300

    exercicios while

--- Guanabara/exercicios/ex068.py
+++ Guanabara/exercicios/ex068.py
@@ -3,17 +3,17 @@ from random import randint
 count = 0
 while True:
     jogada = randint(0, 10)
-    num = str(input('par ou ímpar? [P/I]: ')).upper()
-
-    if num == "P" and jogada % 2 == 0:
-        print('Parabéns, você venceu!')
+    escolha = str(input('par ou ímpar? [P/I]: ')).upper()
+    num = int(input('Digite sua jogada: '))
+    calc = num + jogada 
+    if escolha == "P" and calc % 2 == 0:
+        print(f'Parabéns, você venceu! você jogou: {num} e o computador jogou: {jogada}')
         count += 1
-    elif num == "i":
-        if num == 1:
-            print('Parabéns, você venceu!')
-            count += 1
-        elif (num + 3) % 2 ==0:
-            print('Parabéns, você venceu!')
-            count += 1
+    elif escolha == "I" and calc % 2 == 1:
+        print(f'Parabéns, você venceu! você jogou: {num} e o computador jogou: {jogada}')
+        count += 1
     else:
-        print(f'Você perdeu e ganhou {count} vezes seguidas')
+        print(f'Você perdeu! você jogou: {num} e o computador jogou: {jogada}')
+        break
+
+print(f'Fim de jogo! você jogou {count} vezes')
