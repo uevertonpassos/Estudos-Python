739d241f1430110a1f57fdfa01eaea4673c69b73
Ueverton Passos <uevertonpassos96@gmail.com>
2023-02-02 11:14:14 -0300

    atualizações while

--- Guanabara/exercicios/ex060.py
+++ Guanabara/exercicios/ex060.py
@@ -4,5 +4,7 @@ import math
 n = int(input("Digite o valor para o cálculo de fatorial: "))
 while True:
     print(f"o fatorial de {n} é {math.factorial(n)}")
-    break
+ 
+    n = int(input("Digite o valor para o cálculo de fatorial: "))
+  
 
