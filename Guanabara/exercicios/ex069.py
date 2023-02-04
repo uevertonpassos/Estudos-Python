542f0b6c2476f9e4a21bbb69af8e2e305d032fcd
Ueverton Passos <uevertonpassos96@gmail.com>
2023-02-04 16:10:46 -0300

    commitando

--- Guanabara/exercicios/ex069.py
+++ Guanabara/exercicios/ex069.py
@@ -1,33 +1,28 @@
 from datetime import date
-from time import sleep
-anoAtual = date.today().year
-maiores = 0
-mulheresMenores = 0
-homensCadastrados = 0
 
+ano_atual = date.today().year
+maiores = 0
+mulheres_menores = 0
+homens_cadastrados = 0
 
 while True:
     idade = int(input("Digite a sua idade: "))
-    if idade >=18:
-        maiores += 1  
-    sexo = str(input("Digite o seu sexo [M/F]: ")).upper().strip()[0]
-    print("Cadastrado com sucesso! ")
-    escolha = str(input("Deseja cadastrar mais alguém? [S/N]: ")).upper().strip()[0]
-    if escolha == "S":
-        print("certo, recomeçando! ")
-    if sexo == "F" and idade <= 20:
-        mulheresMenores += 1              
-    elif sexo == "M":
-        homensCadastrados += 1
-        sleep(1)
+    if idade >= 18:
+        maiores += 1
     else:
-        print("=======================================")
-        print(f'{maiores} pessoas são maiores de idade')
-        print(f'foram cadastrados {homensCadastrados} homens')
-        print(f'temos {mulheresMenores} mulheres com menos de vinte anos')
-        print("=======================================")
-    
-    
+        sexo = input("Digite o seu sexo (M/F): ").strip().upper()[0]
+        while sexo not in ["M", "F"]:
+            sexo = input("Por favor, digite M ou F para o sexo: ").strip().upper()[0]
+        if sexo == "F" and idade <= 20:
+            mulheres_menores += 1
+        elif sexo == "M":
+            homens_cadastrados += 1
+    novo_cadastro = input("Deseja cadastrar mais alguém (S/N)? ").strip().upper()[0]
+    if novo_cadastro != "S":
+        break
 
-
-
+print("=======================================")
+print(f"{maiores} pessoas são maiores de idade")
+print(f"Foram cadastrados {homens_cadastrados} homens")
+print(f"Temos {mulheres_menores} mulheres com menos de vinte anos")
+print("=======================================")
