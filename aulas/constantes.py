86910332e34fa5ff63646f64d9cffdba3e330128
Ueverton Passos <uevertonpassos96@gmail.com>
2023-03-12 08:40:26 -0300

    constantes em python

---
+++ aulas/constantes.py
@@ -0,0 +1,13 @@
+#tentando me ensinar constantes em python
+
+variavel = "Isso é uma variável"
+
+CONSTANTE = "isso é uma constante"
+
+'''
+O python n tem a definição de constantes
+para indicar que uma variável não DEVE ser mudada,
+costumamos coloca-la em letras maiúsculas como no código acima
+'''
+print(variavel)
+print(CONSTANTE)
