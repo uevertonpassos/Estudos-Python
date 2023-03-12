9453528ae90d5e72de5c8721c02e48e4289c97dc
Ueverton Passos <uevertonpassos96@gmail.com>
2023-03-12 10:16:35 -0300

    correções usando try/except

--- aulas/alguns exercicios/ExercicioHoraDoDia.py
+++ aulas/alguns exercicios/ExercicioHoraDoDia.py
@@ -1,8 +1,12 @@
-hora = int(input("Digite a hora: "))
-if hora >= 0 and hora <= 11:
-    print("Bom dia!")
-elif hora >= 12 and hora <= 17:
-    print("Boa tarde!")
-else:
-    print("Boa noite!")
-    
+
+
+try:
+    hora = int(input("Digite a hora: "))
+    if hora >= 0 and hora <= 11:
+        print("Bom dia!")
+    elif hora >= 12 and hora <= 17:
+        print("Boa tarde!")
+    else:
+        print("Boa noite!")
+except:
+    print("Digite apenas números inteiros e mairores que 0")
--- aulas/alguns exercicios/exerciciosIF.py
+++ aulas/alguns exercicios/exerciciosIF.py
@@ -1,7 +1,13 @@
-num = int(input("Digite um número inteiro: "))
 
-if num % 2 == 0:
-    print("O número é par!")
-else:
-    print("O número é ímpar!")
-    
+
+try:
+    num = int(input("Digite um número inteiro: "))
+    if num % 2 == 0:
+        print("O número é par!")
+        
+    else:
+        print("O número é ímpar!")
+        
+
+except:
+    print("Digite apenas numeros")
