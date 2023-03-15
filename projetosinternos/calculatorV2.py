db4bdaddd46b819a73fb8d0fdcae54b2ba7e7b2d
Ueverton Passos <uevertonpassos96@gmail.com>
2023-03-15 19:12:55 -0300

    Tentando não me deprimir

---
+++ projetosinternos/calculatorV2.py
@@ -0,0 +1,15 @@
+while True:   
+    print( '''CALCULADORA!
+
+    <ESCOLHA A SUA OPERAÇÃO>
+
+    Digite a operação que deseja realizar:
+    [1] Adição
+    [2] Subtração   
+    [3] Multiplicação
+    [4] Divisão
+    
+    
+    
+    ''')
+    escolha = int(input("Digite a sua opção: ")
