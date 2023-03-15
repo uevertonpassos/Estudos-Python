b07cf3f583a003ec756f057ccdfd6d541c322909
Ueverton Passos <uevertonpassos96@gmail.com>
2023-03-15 19:33:39 -0300

    SOMA COMPLETADA

--- projetosinternos/calculatorV2.py
+++ projetosinternos/calculatorV2.py
@@ -1,3 +1,5 @@
+
+from time import sleep
 while True:   
     print( '''CALCULADORA!
 
@@ -8,8 +10,19 @@ while True:
     [2] Subtração   
     [3] Multiplicação
     [4] Divisão
-    
-    
-    
     ''')
-    escolha = int(input("Digite a sua opção: ")
+  
+    escolha = int(input("Digite a sua opção: "))
+    if escolha == 1:
+        valores = int(input("Digite a quantidade de valores: "))
+        adicao = 0
+
+        for jogadas in range(0, valores):
+            num = int(input("Digite o valor: "))
+            adicao+=num
+        print(f"a soma de todos os valores foi: {adicao}")
+        print("")
+        print("")
+        Jogar_novamente = input("Deseja realizar outra operação? [S/N]")
+
+
