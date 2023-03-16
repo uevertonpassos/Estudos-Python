d9233105865d8e57e9d3d96d5a5b6c5ca68c4542
Ueverton Passos <uevertonpassos96@gmail.com>
2023-03-16 09:00:33 -0300

    Operações básicas finalizadas e funcionais

--- projetosinternos/calculatorV2.py
+++ projetosinternos/calculatorV2.py
@@ -12,17 +12,82 @@ while True:
     [4] Divisão
     ''')
   
-    escolha = int(input("Digite a sua opção: "))
+    # Aqui se inicia o bloco de adição
+
+    escolha = float(input("Digite a sua opção: "))
     if escolha == 1:
         valores = int(input("Digite a quantidade de valores: "))
         adicao = 0
 
         for jogadas in range(0, valores):
-            num = int(input("Digite o valor: "))
+            num = float(input("Digite o valor: "))
             adicao+=num
         print(f"a soma de todos os valores foi: {adicao}")
         print("")
         print("")
-        Jogar_novamente = input("Deseja realizar outra operação? [S/N]")
+        Jogar_novamente = input("Deseja realizar outra operação? [S/N]: ").lower()
+        if Jogar_novamente == "s":
+            pass
+        else:
+            print("Calculadora encerrada")
+            break
+
+    # Aqui se inicia o bloco de subtração
+
+    if escolha == 2:
+        valores = int(input("Digite a quantidade de valores: "))
+        subtracao = float(input("Digite o valor inicial: "))
+        for jogadas in range(0, valores):
+            num = float(input("Digite o valor: "))
+            subtracao-=num
+        print(f"A subtração dos valores vale: {subtracao}")
+        print("")
+        print("")
+        Jogar_novamente = input("Deseja realizar outra operação? [S/N]: ").lower()
+        if Jogar_novamente == "s":
+            pass
+        else:
+            print("Calculadora encerrada")
+
+
+    # Aqui se inicia o bloco de multiplicação
+
+    if escolha == 3:
+        valores = int(input("Digite a quantidade de valores: "))
+        multiplicacao = 1
+        for jogadas in range(0, valores):
+            num = float(input("Digite o valor: "))
+            multiplicacao *= num
+        print(f"a multiplicação dos valores foi: {multiplicacao}")
+        print("")
+        print("")
+        Jogar_novamente = input("Deseja realizar outra operação? [S/N]").lower()
+        if Jogar_novamente == "s":
+            pass
+        else:
+            print("Calculadora encerrada")
+            break
+    
+    # Aqui se inicia o bloco de multiplicação
+
+    if escolha == 4:
+        valores = int(input("Digite a quantidade de valores: "))
+        divisao = float(input("Digite o primeiro valor: "))
+        for jogadas in range(0, valores):
+            num = float(input("Digite o valor: "))
+            divisao /= num
+        print(f"a divisão dos valores sera: {divisao:.2f}")
+        print("")
+        print("")
+        Jogar_novamente = input("Deseja realizar outra operação? [S/N]: ").lower()
+        if Jogar_novamente == "s":
+            pass
+        else:
+            print("Calculadora encerrada")
+            break
+
+
+
+
 
 
