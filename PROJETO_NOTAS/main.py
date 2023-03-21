bad95b211ea936ba4b99162228d373cc17c98599
Ueverton Passos <uevertonpassos96@gmail.com>
2023-03-21 16:49:07 -0300

    escopo projeto notas

---
+++ PROJETO_NOTAS/main.py
@@ -0,0 +1,7 @@
+'''
+criar uma interface que receba nome e  notas dos alunos
+e que encaminhe para uma tabela de excel calculando a média
+
+o intuito é facilitar a vida da minha mãe e de outros professores
+
+'''
