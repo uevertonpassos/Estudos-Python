801cda77bd155cfaa48dc88d004a67e6f19bd935
Ueverton Passos <uevertonpassos96@gmail.com>
2023-03-27 17:36:38 -0300

    Atualizando app

--- PROJETO_NOTAS/main.py
+++ PROJETO_NOTAS/main.py
@@ -5,3 +5,22 @@ e que encaminhe para uma tabela de excel calculando a média
 o intuito é facilitar a vida da minha mãe e de outros professores
 
 '''
+
+'''
+[IDEIAS]
+
+o professor deve ter a possibilidade de escolher entre várias planilhas 
+para selecionar a que le quer pois muitos professores dão aulas em mais 
+de uma escola e mais de uma sala de aula
+
+
+o programa deve reconhecer se o nome do aluno já foi digitado e  perguntar de qual avaliação é aquela nota, assim pode posicionala no local certo
+
+
+[NOMES?]
+
+ClassTable
+
+Class Coordenate
+
+'''
