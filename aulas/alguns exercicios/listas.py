feea07e230b9d6e5300bd02e2dc1697ec3d683f0
Ueverton Passos <uevertonpassos96@gmail.com>
2023-03-28 17:26:13 -0300

    estudando sobre  listas

--- aulas/alguns exercicios/listas.py
+++ aulas/alguns exercicios/listas.py
@@ -11,3 +11,15 @@ print(lista)
 #adicionando um elemento ao final da lista
 lista.append(3000)
 print(lista)
+
+#deletando indices da lista
+del lista[2]
+print(lista)
+
+#inserindo valores na lista, primeiro passsar o indice, depois o que usar para substituição
+lista.insert(0, 22)
+print(lista)
+
+#limpando a lista
+lista.clear()
+print(lista)
