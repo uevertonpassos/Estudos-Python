f038f95c7b2c6f09644ec4c9250775c8b1718ecd
Ueverton Passos <uevertonpassos96@gmail.com>
2023-03-28 17:14:03 -0300

    aprendendo um pouco sobre listas

---
+++ aulas/alguns exercicios/listas.py
@@ -0,0 +1,13 @@
+lista = [10, 20, 30, 40]
+
+#mudando valor da lista pelo índice
+lista[2] = 300
+print(lista)
+
+#apagando o último elemento da lista
+lista.pop()
+print(lista)
+
+#adicionando um elemento ao final da lista
+lista.append(3000)
+print(lista)
---
+++ aulas/alguns exercicios/parImpar.py
@@ -0,0 +1,6 @@
+#Escreva um programa que leia um número inteiro e exiba se ele é par ou ímpar.
+num = int(input("Digite um número: "))
+if num % 2 == 0:
+    print("O número é par")
+else:
+    print("O número é impar")
