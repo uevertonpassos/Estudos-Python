93377b9593896ba46841ecf2de7d6949245421d8
Ueverton Passos <uevertonpassos96@gmail.com>
2023-04-11 10:07:18 -0300

    Treinando listas

---
+++ PythonBrasil/list_01.py
@@ -0,0 +1,14 @@
+#Faça um Programa que leia um vetor de 5 números inteiros e mostre-os. 
+
+count = 0
+list_of_numbers = []
+
+while count <= 5:
+    number = int(input("Enter an integer value: "))
+    list_of_numbers.append(number)
+    count+=1
+    
+print("")
+print("Thank you! This is the list of numbers: ")
+print(list_of_numbers)
+
