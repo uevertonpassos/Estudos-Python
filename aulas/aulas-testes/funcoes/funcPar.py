784dbccfc9c2a8d80bcebdad13642ab638f20e89
Ueverton Passos <uevertonpassos96@gmail.com>
2023-04-17 16:27:11 -0300

    treinando funções

--- aulas/aulas-testes/funcoes/funcPar.py
+++ aulas/aulas-testes/funcoes/funcPar.py
@@ -1,4 +1,16 @@
-lista [ 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+#Escreva uma função que receba uma lista de números e retorne apenas os números pares.
+
+lista = [ 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+lista_par = []
 
 def verificar_par(lista):
-    par = 
+    for numero in lista:
+        if numero % 2 == 0:
+            lista_par.append(numero)
+
+
+verificar_par(lista)
+print(f"os numeros pares foram {lista_par}")
+
