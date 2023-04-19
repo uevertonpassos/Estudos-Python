f56c63f6b36f24aa24dc16ec8126eebf1fadb507
Ueverton Passos <uevertonpassos96@gmail.com>
2023-04-19 08:14:30 -0300

    corrigido

--- Guanabara/exercicios/ex072.py
+++ Guanabara/exercicios/ex072.py
@@ -3,15 +3,8 @@
 # Definindo a lista de números por extenso
 numeros = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 
-try:
-    while True:
-        numero = int(input("Digite um número de 0 a 20: "))
-
-        print(f'O número {numero} por extenso é: {numeros[numero]}\n')
-
-except:
-    print("Digite algo válido\n")
-    while True:
-        numero = int(input("Digite um número de 0 a 20: "))
-
-        print(f'O número {numero} por extenso é {numeros[numero]}')
+numero = int(input("Digite um número de 0 a 20: "))
+while numero < 0 or numero > 20:
+    numero = int(input("Digite um número de 0 a 20: "))
+    
+print(f'O número {numero} por extenso é: {numeros[numero]}\n')
