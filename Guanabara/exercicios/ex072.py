ed3984b80f1a7de771b23e527719ed8d278a4657
Ueverton Passos <uevertonpassos96@gmail.com>
2023-04-20 10:06:00 -0300

    exercicios com tupla

--- Guanabara/exercicios/ex072.py
+++ Guanabara/exercicios/ex072.py
@@ -3,8 +3,12 @@
 # Definindo a lista de números por extenso
 numeros = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 
-numero = int(input("Digite um número de 0 a 20: "))
-while numero < 0 or numero > 20:
+
+while True:
     numero = int(input("Digite um número de 0 a 20: "))
+    if  0 <= numero <=20:
+        break
+    print('tente novamente')
+        
     
 print(f'O número {numero} por extenso é: {numeros[numero]}\n')
