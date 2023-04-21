15c4ba6ffa9b7d1547a5d6c01657543b7a739f4e
Ueverton Passos <uevertonpassos96@gmail.com>
2023-04-21 10:11:41 -0300

    lista numa tupla só

---
+++ Guanabara/exercicios/ex076.py
@@ -0,0 +1,7 @@
+produtos = ('Batata', 3.50, 'Frango', 16.50, 'Arroz', 6.90, 'Feijão', 8.10)
+
+print('TABELA DE PREÇOS')
+print(f'{produtos[0]}.....................R${produtos[1]:.2f} Reais')
+print(f'{produtos[2]}.....................R${produtos[3]:.2f} Reais')
+print(f'{produtos[4]}......................R${produtos[5]:.2f} Reais')
+print(f'{produtos[6]}.....................R${produtos[7]:.2f} Reais')
