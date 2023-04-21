27ec6a006554fe8aa4d5e0d0f2a6e2f32de9b02d
Ueverton Passos <uevertonpassos96@gmail.com>
2023-04-21 09:41:34 -0300

    exagerando em tupla

---
+++ Guanabara/exercicios/ex075.py
@@ -0,0 +1,19 @@
+minha_tupla = ()
+lista_par = ()
+
+for i in range(0, 4):
+    numero = int(input("Digite um valor: "))
+    if numero % 2 == 0:
+        lista_par+=(numero,)
+    minha_tupla += (numero,)
+
+
+print('')
+print('')
+print(f'O número 9 apareceu {minha_tupla.count(9)} vezes')
+if 3 in minha_tupla:
+    print(f'O primeiro número 3 apareceu em {minha_tupla.index(3)}')
+else:
+    pass
+print(f'Numeros pares: {lista_par}')
+
