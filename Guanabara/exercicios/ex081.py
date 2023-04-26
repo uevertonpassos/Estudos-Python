d08a852a67c415a625c67789b9a36e86008fa258
Ueverton Passos <uevertonpassos96@gmail.com>
2023-04-26 17:46:15 -0300

    corrigindo

---
+++ Guanabara/exercicios/ex081.py
@@ -0,0 +1,16 @@
+lista = []
+
+while True:
+    confirm = input('Deseja adicionar um valor a lista? [S/N]: ').lower()
+    if confirm == 's':
+        numero = int(input('Digite um valor: '))
+        lista.append(numero)
+    else:
+        print(f'Foram digitados {len(lista)} números')
+        print(f'Aqui está a lista ordenada de forma decrescente {sorted(lista, reverse=True)}')
+        #prestar atenção no método reverse
+        if 5 in lista:
+            print('O numero 5 foi colocado na lista')
+        else:
+            print('O número 5 não apareceu')
+        break
