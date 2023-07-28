d7ec73afb17567a9a972fe1570eeba8fd159e723
Ueverton Passos <uevertonpassos96@gmail.com>
2023-07-28 15:44:18 -0300

    corrigindo

--- PythonBrasil/extras/teste_lista.py
+++ PythonBrasil/extras/teste_lista.py
@@ -1,4 +1,9 @@
-# Lista para armazenar as pessoas (nome, peso)
+#faça um programa que leia o nome e peso de várias pessoas guardando tudo em uma lista, No final mostre:
+
+# a) quantas pessoas foram cadastradas
+# b) uma listagem com as pessoas mais pesadas
+# c) uma listagem com pessoas mais leves.
+
 pessoas = []
 
 while True:
@@ -6,15 +11,15 @@ while True:
     peso = float(input('Digite o peso da pessoa: '))
     sair = input("Digite 0 para sair ou 1 para continuar: ")
     
-    pessoas.append((nome, peso))  # Armazenar nome e peso como uma tupla
+    pessoas.append((nome, peso))  
     
     if sair == "0":
         break
 
-# Contagem de pessoas cadastradas
+
 num_pessoas = len(pessoas)
 
-# Filtrar as pessoas mais pesadas e mais leves
+
 mais_pesadas = [pessoa[0] for pessoa in pessoas if pessoa[1] >= 100]
 mais_leves = [pessoa[0] for pessoa in pessoas if pessoa[1] < 100]
 
