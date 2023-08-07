b949b5146630dafa4e94efc71a54bfb488c16e37
Ueverton Passos <uevertonpassos96@gmail.com>
2023-08-07 19:01:09 -0300

    Agatha the book is on the table

---
+++ uevertonpassos/voz.py
@@ -0,0 +1,35 @@
+import speech_recognition as sr
+import subprocess
+
+def execute_command(command):
+    try:
+        subprocess.run(command, shell=True, check=True)
+    except subprocess.CalledProcessError:
+        print("Erro ao executar o comando:", command)
+
+def main():
+    recognizer = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Aguardando o comando 'AGATHA'...")
+        recognizer.adjust_for_ambient_noise(source)
+        
+        while True:
+            audio = recognizer.listen(source)
+            
+            try:
+                recognized_text = recognizer.recognize_google(audio).lower()
+                print("Comando detectado:", recognized_text)
+                
+                if "agatha" in recognized_text:
+                    if "open the terminal" in recognized_text:
+                        execute_command("start cmd")  
+                    
+                    
+            except sr.UnknownValueError:
+                print("Não foi possível entender o áudio.")
+            except sr.RequestError as e:
+                print("Erro na solicitação ao Google Speech Recognition:", e)
+
+if __name__ == "__main__":
+    main()
