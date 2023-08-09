d2016c93607bf2bb8e11525c07d32bb07dae1c06
Ueverton Passos <uevertonpassos96@gmail.com>
2023-08-09 11:53:13 -0300

    mudando um pouco os valores  e variáveis

--- aulas/aulas-testes/funcoes/funcaoDois.py
+++ aulas/aulas-testes/funcoes/funcaoDois.py
@@ -1,12 +1,12 @@
 # Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos. 
 
 def soma(num1, num2, num3):
-    print(num1 + num2 + num3)
+    somando = (num1 + num2 + num3)
+    print(f"A soma entre os três valores é: {somando}")
 
 numeroUm = int(input("Digite um valor: "))
 numeroDois = int(input("Digite outro valor: "))
 numeroTres = int(input("Digite o terceiro valor: ")) 
-print(f"A soma entre {numeroUm}, {numeroDois} e {numeroTres} é: ")
 soma(numeroUm, numeroDois, numeroTres)
 
 
