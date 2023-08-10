9441f665e06de7a9f3608d677e86109604bc5f7a
Ueverton Passos <uevertonpassos96@gmail.com>
2023-08-10 11:59:06 -0300

    aiai

--- aulas/aulas-testes/funcoes/teste.java
+++ aulas/aulas-testes/funcoes/teste.java
@@ -1,5 +1,5 @@
 public class teste{
     public static void main(String[] args){
-        System.out.println("HELLO, WORLD!");
+        System.out.println("tomar no cu esse caralho de bolsa em java");
     }
 }
