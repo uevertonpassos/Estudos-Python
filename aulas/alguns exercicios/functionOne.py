ed3a34579d02dbf2351487f8ded9276d3e58bc16
Ueverton Passos <uevertonpassos96@gmail.com>
2023-09-04 22:00:35 -0300

    brincando com functions

---
+++ aulas/alguns exercicios/functionOne.py
@@ -0,0 +1,8 @@
+# Escreva uma função em Python que exiba a mensagem "Olá, Mundo!" quando chamada.
+
+def OlaMundo():
+    print('Olá, mundo!')
+
+
+print('Gostaria de dizer:')
+OlaMundo()
---
+++ aulas/alguns exercicios/tweet.py
@@ -0,0 +1,5 @@
+T = input("Digite algo: ")
+if len(T) <= 140:
+  print("TWEET")
+else:
+  print("MUTE")
