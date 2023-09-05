ae95ca815f4fdad71e107396ddc2378d03b206e1
Ueverton Passos <uevertonpassos96@gmail.com>
2023-09-04 22:14:15 -0300

    terceiro exercicio de funções

---
+++ aulas/alguns exercicios/functionTrês.py
@@ -0,0 +1,8 @@
+#  Desenvolva uma função que receba o nome de uma pessoa como argumento e exiba uma mensagem de boas-vindas personalizada.
+
+def BoasVindas(nome):
+    print(f'Bem vindo(a) {nome}! ')
+
+nome = input('Digiter o seu nome, por favor: ')
+
+BoasVindas(nome)
