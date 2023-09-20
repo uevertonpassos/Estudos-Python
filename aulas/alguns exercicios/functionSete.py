469da60475676e0898df74fcaf7b3063d0eb5321
Ueverton Passos <uevertonpassos96@gmail.com>
2023-09-20 12:14:32 -0300

    iiih

---
+++ aulas/alguns exercicios/functionSete.py
@@ -0,0 +1,12 @@
+def astrid():
+    if  kiss == 'S':
+        print("aleluia")
+    else:
+        print("pelo amor de deus beija esse garoto")
+    
+
+
+kiss = input('você quer beijar esse garoto? [S]im [N]ão: ').upper()
+astrid()
+
+
