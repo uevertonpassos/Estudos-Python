654707de1074dd630f0af800b6eb329e591e0116
Ueverton Passos <uevertonpassos96@gmail.com>
2023-11-01 17:36:23 -0300

    aprendendo tuplas

---
+++ aulas/tuplas/tuplas.py
@@ -0,0 +1,5 @@
+# estudando tuplas 
+minha_tupla = (1, 2, 1,1,3, 'a', 'b', 'c')
+outra_tupla = 4, 5, 6
+print(minha_tupla[0])  # Isso imprimirá 1
+print(set(minha_tupla)) #treinando set, retira itens duplicados
