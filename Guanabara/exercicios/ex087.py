48af70db9af56446c273526ac1e6bf55781a796f
Ueverton Passos <uevertonpassos96@gmail.com>
2023-11-06 15:25:08 -0300

    exercicios guanabara

--- Guanabara/exercicios/ex087.py
+++ Guanabara/exercicios/ex087.py
@@ -1,9 +1,7 @@
 '''
 Aprimore o desafio anterior,mostrando final:                                                    A) A soma de todos os valores pares digitados.                                                                                      B) A soma dos valores da terceira coluna.                                                                                         C) O maior valor da segunda linha.
 '''
-
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-
 
 for i in range(3):
     for j in range(3):
