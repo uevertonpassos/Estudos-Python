9eb9cc9cd265ecd23aaf2b1a18b3ec6b707f142e
Ueverton Passos <uevertonpassos96@gmail.com>
2023-11-06 16:19:11 -0300

    treinando funções

---
+++ aulas/alguns exercicios/testando_func.py
@@ -0,0 +1,9 @@
+def soma(x,y):
+    resultado = x + y
+    return(resultado)
+
+num = int(input("Digite um valor: "))
+numDois = int(input("Digite outro valor: "))
+
+resultado_soma = soma(num, numDois)
+print(f"O resultado da soma dos valores é: {resultado_soma}")
